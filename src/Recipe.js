544f40b5fc789b97f1a48aba20ca216d09d211e3
SonPhan <phantruongson96@gmail.com>
2021-02-04 17:06:08 -0500

    final

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import styles from './recipe.module.css';
 
-const Recipe = ({ title, calories, image }) => {
+const Recipe = ({ title, calories, image, ingredients }) => {
     return(
-        <div>
+        <div className={styles.recipe}>
             <h1>{title}</h1>
-            <p>{calories}</p>
-            <img src={image} alt="img recipe" />
+            <ol>
+                {ingredients.map((ingredient,i) => (
+                    <li key={Math.random()}>{ingredient.text}</li>
+                ))}
+            </ol>
+            <p>Carlories: {calories.toFixed(2)}</p>
+            <img src={image} className={styles.image} alt="img recipe" />
         </div>
+        
     );
 };
 
