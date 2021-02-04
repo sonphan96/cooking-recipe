b73b9d2af7e2e6c20d417f345f50049e407b5306
SonPhan <phantruongson96@gmail.com>
2021-02-04 14:34:28 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from 'react';
+import './App.css';
+
+const APP_ID = process.env.REACT_APP_ID;
+const APP_KEY = process.env.REACT_APP_KEY;
+
+const App = () => {
+
+
+  useEffect(() => {
+    getRecipes()
+  }, [])
+
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = await response.json();
+    console.log(data)
+  }
+
+  return (
+    <div className='App'>
+      <form className='search-form'>
+        <input className='search-bar' type='text' />
+        <button className='search-button' type='submit'>
+          Search
+        </button>
+      </form>
+    </div>
+  )
+}
+
+export default App;
